7c538c4be2136e7cb5afb5c8bffee1b39413b18a
huyleminh <leminhhuy.hcmus@gmail.com>
2020-04-23 12:19:22 +0700

    main.cpp

--- ktlt-clc2-group11/Student-management-system/main.cpp
+++ ktlt-clc2-group11/Student-management-system/main.cpp
@@ -1,10 +1,9 @@
 /*
-This file is run the program and it calls run() function only, please don't change anything in this file.
+This file is run the program only, please don't change anything in this file.
 */
 #include "Header.h"
 
 int main() {
-	//run();
 
 	return 0;
 }
