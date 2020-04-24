ac8783fdf99161236c15d2443d2fb260fe9a668c
huyleminh <leminhhuy.hcmus@gmail.com>
2020-04-25 01:56:58 +0700

    Create Staff.h

---
+++ ktlt-clc2-group11/Student-management-system/Staff.h
@@ -0,0 +1,11 @@
+#ifndef _STAFF_H_
+#define _STAFF_H_
+
+#include "Header.h"
+
+void showStaffMenu();
+void classAndStudentMode();
+void importStudentsFromCSV(Student*& students, int& nStudent);
+void createUserPassword(Student* students, const int nStudent);
+
+#endif
