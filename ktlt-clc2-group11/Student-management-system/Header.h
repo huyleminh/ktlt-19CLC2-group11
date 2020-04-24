835fea7d93ed1b8ebd96d9b88b4ff9da97c5eb80
huyleminh <leminhhuy.hcmus@gmail.com>
2020-04-25 01:56:45 +0700

    Update Header.h

--- ktlt-clc2-group11/Student-management-system/Header.h
+++ ktlt-clc2-group11/Student-management-system/Header.h
@@ -6,6 +6,40 @@
 #include <string>
 using namespace std;
 
+struct Date {
+	int day;
+	int month;
+	int year;
+};
+
+struct User {
+	string username;
+	string password;
+};
+
+struct Staff {
+	User user;
+	string name;
+};
+
+struct Lecturer {
+	User user;
+	string name;
+};
+
+struct Student_User {
+	User user;
+	string name;
+};
+
+struct Student{
+	string ID;
+	string fullName;
+	string gender;
+	string DoB;
+	string classID;
+};
+
 inline bool isFileOpen(ifstream &fin, const char* filename) {
 	fin.open(filename);
 	if (!fin.is_open()) {
