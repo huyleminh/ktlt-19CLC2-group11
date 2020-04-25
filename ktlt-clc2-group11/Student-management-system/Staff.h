1369f98f13a07bc47400d8d27c129c225148c304
huyleminh <leminhhuy.hcmus@gmail.com>
2020-04-25 21:54:06 +0700

    Update Staff.h

--- ktlt-clc2-group11/Student-management-system/Staff.h
+++ ktlt-clc2-group11/Student-management-system/Staff.h
@@ -3,10 +3,16 @@
 
 #include "Header.h"
 
-void showStaffMenu();
+void showStaffMenu(User& user);
+
+//1. Class and student.
 void classAndStudentMode();
 void importStudentsFromCSV(Student*& students, int& nStudent);
 void createUserPassword(Student* students, const int nStudent);
 void viewListClasses(ifstream& data, int& nClasses);
+//5. Change password.
+void changeStaffPassword(User& user); 
+//6. View profile infomation.
+//This function is located in Header.h
 
 #endif
