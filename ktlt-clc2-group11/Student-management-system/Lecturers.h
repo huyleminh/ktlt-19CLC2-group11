1bda28d51bc25ed5d682e98f1099fe2a4e42492f
huyleminh <leminhhuy.hcmus@gmail.com>
2020-04-25 21:54:02 +0700

    Update Lecturers.h

--- ktlt-clc2-group11/Student-management-system/Lecturers.h
+++ ktlt-clc2-group11/Student-management-system/Lecturers.h
@@ -3,6 +3,18 @@
 
 #include "Header.h"
 
-void showLecturerMenu();
+void showLecturerMenu(User& user);
+
+//1. View list of courses in the current semester.
+//2. View list of students of a course.
+//3. View attendance list of a course.
+//4. Edit an attendance.
+//5. Import scoreboard of a course(midterm, final, lab, bonus) from a csv file.
+//6. Edit grade of a student.
+//7. View a scoreboard.
+//8. Change password.
+void changeLecPassword(User& user);
+//9. View profile infomation.
+//this function is located in Header.h
 
 #endif
