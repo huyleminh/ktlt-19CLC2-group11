63acbe2408dbc0e9009e7b1adb50e11a50420237
huyleminh <leminhhuy.hcmus@gmail.com>
2020-04-25 10:18:02 +0700

    Update AllRole.h

--- ktlt-clc2-group11/Student-management-system/AllRole.h
+++ ktlt-clc2-group11/Student-management-system/AllRole.h
@@ -4,9 +4,11 @@
 #include "Header.h"
 
 void menu();
+
 void login(User user, unsigned int& loginMode);
-bool isStaffLogin(User loginUser);
-bool isLecturerLogin(User loginUser);
-bool isStudentLogin(User loginUser);
+
+bool isStaffLogin(const User loginUser);
+bool isLecturerLogin(const User loginUser);
+bool isStudentLogin(const User loginUser);
 
 #endif
