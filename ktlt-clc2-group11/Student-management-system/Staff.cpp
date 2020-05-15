c65eefd9c47eac225ea93e44e5f4b9968baa5e20
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2020-05-15 21:31:31 +0700

    add filterStudentToClass

--- ktlt-clc2-group11/Student-management-system/Staff.cpp
+++ ktlt-clc2-group11/Student-management-system/Staff.cpp
@@ -122,6 +122,36 @@ void importStudentsFromCSV(Student*& students, int& nStudent) {
 	fout.close();
 }
 
+void filterStudentToClass(string filename)
+{
+	ifstream in(filename);
+	int nStudents;
+	if (!in.is_open())
+	{
+		cout << "Cant open file" << endl;
+		return;
+	}
+
+	in >> nStudents;
+
+	Student* a = new Student[nStudents];
+
+	importStudentsFromCSV(a, nStudents);
+
+	for (int i = 0; i < nStudents; i++)
+	{
+		string classFile = a[i].classID + "-Students.txt";
+		ofstream fout(classFile, ofstream::app);
+		if (!fout.is_open())
+		{
+			cout << "Cant create file" << endl;
+			return;
+		}
+		fout << a[i].ID << endl << a[i].fullName << endl << a[i].DoB << endl << a[i].gender << endl;
+		fout.close();
+	}
+}
+
 void createUserPassword(Student* students, const int nStudent) {
 	ofstream fout;
 	fout.open("StudentUsers.txt");
