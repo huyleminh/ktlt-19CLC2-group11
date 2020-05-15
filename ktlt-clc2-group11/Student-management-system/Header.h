d66962886dde672c845f5ef2a1556490f4c334d5
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2020-05-15 21:31:46 +0700

    add some changes

--- ktlt-clc2-group11/Student-management-system/Header.h
+++ ktlt-clc2-group11/Student-management-system/Header.h
@@ -10,6 +10,20 @@ struct Date {
 	int day;
 	int month;
 	int year;
+	void inputDay() {
+		cout << "Day: ";
+		cin >> day;
+		cout << "Month: ";
+		cin >> month;
+		cout << "year: ";
+		cin >> year;
+	}
+	void deleteDay() {
+		day = 0; month = 0; year = 0;
+	}
+	void printDay() {
+		cout << day << "/" << month << "/" << year << endl;
+	}
 };
 
 struct User {
@@ -33,12 +47,74 @@ struct Student_User {
 	string name;
 };
 
-struct Student{
+struct Student {
 	string ID;
 	string fullName;
 	string gender;
 	string DoB;
 	string classID;
+};
+
+struct Time
+{
+	string dayOfWeek;
+	string startHour;
+	string startMin;
+	string endHour;
+	string endMin;
+	void inputTime()
+	{
+		cout << "day of week: ";
+		cin >> dayOfWeek;
+
+		cout << "start hour: ";
+		cin >> startHour;
+
+		cout << "start min: ";
+		cin >> startMin;
+
+		cout << "end hour: ";
+		cin >> endHour;
+
+		cout << "end min: ";
+		cin >> endMin;
+	}
+	void deleteTime()
+	{
+		dayOfWeek = "";
+		startHour = "";
+		endHour = "";
+		startMin = "";
+		endMin = "";
+	}
+	void printTime()
+	{
+		cout << "Day of week: " << dayOfWeek << endl;
+		cout << startHour << ":" << endHour << endl;
+		cout << startMin << ":" << endMin << endl;
+	}
+};
+
+struct Course
+{
+	string ID;
+	string name;
+	string classID;
+	string lecAccount;
+	Date startDate;
+	Date endDate;
+	Time courseTime;
+	string room;
+};
+
+struct Semester {
+	int numberOfCourses;
+	Course* coursesArray;
+};
+
+struct academicYear {
+	int startYear;
+	Semester semesterArray[3];
 };
 
 inline bool isFileOpen(ifstream &fin, const char* filename) {
