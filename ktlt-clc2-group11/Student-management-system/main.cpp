2782f43aabac04a4282689cce8303f6e6646291d
huyleminh <leminhhuy.hcmus@gmail.com>
2020-05-30 01:05:02 +0700

    Update main.cpp

--- ktlt-clc2-group11/Student-management-system/main.cpp
+++ ktlt-clc2-group11/Student-management-system/main.cpp
@@ -1,9 +1,26 @@
 /*
-This file is run the program only, please don't change anything in this file.
+	*COURSE: PROGRAM TECHNIQUES
+	*CLASS: 19CLC2
+	*GROUP: GA CHIEN
+	*ID: 11
+
+	This is the first version of Student Manegement System, built on development environment,
+	it will be updated some features and public soon.
+
 */
+/*
+	We notice that in main function, it's only renders menu() function, and menu() will render the program.
+*/
+
 #include "Header.h"
+#include "AllRole.h"
+#include "Staff.h"
+#include "Lecturers.h"
+#include "Student.h"
+
 
 int main() {
-
+	cout << "STUDENTS MANAGEMENT SYSTEM DEMO VERSION 0.2" << endl;
+	menu();
 	return 0;
 }
