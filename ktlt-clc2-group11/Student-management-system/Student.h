4fcac9f8db8013df7472d625d2b601e364d8d5a2
huyleminh <leminhhuy.hcmus@gmail.com>
2020-05-30 01:05:11 +0700

    Update Student.h

--- ktlt-clc2-group11/Student-management-system/Student.h
+++ ktlt-clc2-group11/Student-management-system/Student.h
@@ -3,15 +3,21 @@
 
 #include "Header.h"
 
+/*Main student menu*/
+
 void showStudentMenu(User& user);
 
 //1. Check-in.
 //2. View check-in result.
 //3. View schedules.
 //4. View your scores of a course.
+
 //5. Change password.
 void changeStudentPassword(User& user);
-//6. View profile infomation.
+
+//6. View student profile infomation.
 //this function is located in Header.h
+
+//7. Logout
 
 #endif 
